4b5d7c97aa4677f9462199565dc64d92443b2841
innoners <innoners@gmail.com>
2017-01-23 17:29:57 +0800

    demo

---
+++ landing.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>OrangeApps | OrangeApps Academy</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="col-md-6 h0 a">
+      <a href=""><div class="g0 fu">
+        <img src="login/adminlog.png" class="aimg">
+      </div></a>
+    </div>
+
+    <div class="col-md-6 h1 b">
+      <a href=""><div class="g1 fd">
+        <img src="login/teacherlog.png" class="aimg">
+      </div></a>
+    </div>
+
+    <div class="col-md-6 h0 c">
+      <a href=""><div class="g0 fu">
+        <img src="login/studentlog.png" class="aimg">
+      </div></a>
+    </div>
+
+    <div class="col-md-6 h1 d">
+      <a href=""><div class="g1 fd">
+        <img src="login/parentlog.png" class="aimg">
+      </div></a>
+    </div>
+
+
+  <style media="screen">
+  	body{
+  		padding: 0px;
+      margin: 0px;
+      background-color: orange;
+  	}
+
+    .h0 {
+      background-color: orange;
+    }
+    .h1 {
+      background-color: #ffc107;
+    }
+  	.aimg{
+  		display: block;
+  		margin-left: auto;
+  		margin-right: auto;
+      height: 100%;
+      position:fixed;
+  	}
+  	.g0{
+  		margin:0;
+  		padding:0;
+  	}
+  	.g1{
+  		margin:0;
+  		padding:0;
+  	}
+  	.row {
+  		padding: 0px !important;
+  		margin: 0px !important;
+  	}
+    .a {
+      width: 23%;
+    }
+    .b {
+      width: 24%;
+    }
+    .c {
+      width: 22%;
+    }
+    .d {
+      width: 26%;
+    }
+  	.col-md-6 {
+  		padding: 0px !important;
+  		margin: 0px !important;
+      float: left;
+  	}
+    ::-webkit-scrollbar {
+        width:7px;
+    }
+
+    ::-webkit-scrollbar-track {
+        -webkit-border-radius:5px;
+        border-radius:5px;
+        background:rgba(0,0,0,0.1);
+    }
+
+    ::-webkit-scrollbar-thumb {
+        -webkit-border-radius:5px;
+        border-radius:5px;
+        background:rgba(0,0,0,0.2);
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background:rgba(0,0,0,0.4);
+    }
+
+    ::-webkit-scrollbar-thumb:window-inactive {
+        background:rgba(0,0,0,0.05);
+    }
+  	</style>
+
+  </body>
+</html>
